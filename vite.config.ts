d9fb9fffcfb2418c1625b63ed31147bfe33f9aa2
shakthichaithanya <shakthits01@gmail.com>
2025-05-25 20:17:25 +0530

    fix the home page url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Professional_Protfolio",
+  base: "/Professional-Protfolio",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
